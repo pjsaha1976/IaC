ac31fbdba4bcea21b5333ec4934818eab0b3fa1c
prosenjit saha <pjsaha1976@gmail.com>
2025-07-30 12:02:16 -0700

    changed azurerm_image resource_group_name to managed_image_resource_group_name

--- terraform-template/main.tf
+++ terraform-template/main.tf
@@ -1,21 +1,5 @@
-# Azure Provider source and version being used
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=4.1.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-  resource_provider_registrations = "none"
 }
 
 resource "azurerm_virtual_network" "main_vnet" {
@@ -121,8 +105,8 @@ resource "azurerm_availability_set" "main_avset" {
 }
 
 data "azurerm_image" "packer_image" {
-  name                              = "packer-ubuntu-server"
-  managed_image_resource_group_name = "Azuredevops"
+  name                = "packer-ubuntu-server"
+  resource_group_name = "pkr-Resource-Group-trupcmzts4"
 }
 
 resource "azurerm_linux_virtual_machine" "main_vm" {
