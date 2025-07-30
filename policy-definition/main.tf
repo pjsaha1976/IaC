28b350fbb8269747827c2145ca10e4280f1d5a45
prosenjit saha <pjsaha1976@gmail.com>
2025-07-30 06:43:29 -0700

    removed resource group fgrom tag compliancy policy

--- policy-definition/main.tf
+++ policy-definition/main.tf
@@ -1,9 +1,29 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  resource_provider_registrations = "none"
+}
+
 resource "azurerm_policy_definition" "require_tags_on_resources" {
   name         = var.policy_name
   policy_type  = "Custom"
   mode         = "Indexed"
-  display_name = "Require tags on all resources (except resource groups)"
-  description  = "Ensures that all indexed resources except resource groups have tags. Denies deployment if tags are missing."
+  display_name = "Require tags on all resources"
+  description  = "Ensures that all indexed resources have tags. Denies deployment if tags are missing."
   policy_rule  = <<POLICY_RULE
 {
   "if": {
