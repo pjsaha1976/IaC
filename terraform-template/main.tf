0dece64a6a29e680834f56e3d348391935110b63
prosenjit saha <pjsaha1976@gmail.com>
2025-07-30 11:34:52 -0700

    terraform script for creating vpc, subnet, nsg, nic, public ip, load balancer, vm

--- terraform-template/main.tf
+++ terraform-template/main.tf
@@ -1,5 +1,21 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  resource_provider_registrations = "none"
 }
 
 resource "azurerm_virtual_network" "main_vnet" {
--- terraform-template/vars.tf
+++ terraform-template/vars.tf
@@ -21,3 +21,23 @@ variable "vm_count" {
   type        = number
   default     = 1
 }
+
+variable "client_id" {
+  description = "The user id of the Service Principal identity that terraform will run under"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The password/client_secret of the Service Principal identity that terraform will run under"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "The tenant id of the Service Principal identity that terraform will run under"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "The subscription id of that is in scope for this Azure Policy Definition"
+  type        = string
+}
