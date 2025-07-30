4ff0715ee153ea679d34c1fd4839748c77a59c9f
prosenjit saha <pjsaha1976@gmail.com>
2025-07-30 10:33:59 -0700

    terraform script for creating vpc, subnet, nsg, nic, public ip, load balancer, vm

--- terraform-template/main.tf
+++ terraform-template/main.tf
@@ -87,7 +87,6 @@ resource "azurerm_lb" "main_lb" {
 resource "azurerm_lb_backend_address_pool" "main_bepool" {
   name                = "${var.prefix}-bepool"
   loadbalancer_id     = azurerm_lb.main_lb.id
-  resource_group_name = "Azuredevops"
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "main_nic_lb_assoc" {
